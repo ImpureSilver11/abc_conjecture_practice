1232bca0c71f9edee6a27e8dd978325ed7699d1d
ImpureSilber11 <take1931032@gmail.com>
2022-04-12 11:34:15 +0900

    init

---
+++ test.rb
@@ -0,0 +1,36 @@
+# ABC予想
+
+# a + b = c を満たす、互いに素な自然数の組 (a, b, c) に対し、積 abc の互いに異なる素因数の積を d と表す。
+# このとき、任意の ε > 0 に対して、
+# c > d^1+ε
+# を満たす組 (a, b, c, d)を列挙する
+def abc_conjecture
+  result = []
+  (1..1000).each do |a|
+    (a..1000).each do |b|
+      # a + b = c
+      c = a + b
+      # 互いに異なる素因数の積
+      d = (prime_factor_decomposition(a) + prime_factor_decomposition(b) + prime_factor_decomposition(c)).uniq
+      if c > d.inject(:*)
+        result.push([a, b, c, d])
+      end
+    end
+  end
+  result
+end
+
+# 素因数分解を行う
+# e.g. 18 => [2, 3 ,3]
+def prime_factor_decomposition(number)
+  result = []
+  (2..number).map do |i|
+    while number % i == 0
+      number = number / i
+      result << i
+    end
+  end
+  result.flatten.compact
+end
+
+pp abc_conjecture
